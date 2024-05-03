46a85558d639458af8dd82ec3a211abe90527645
anand kumar <ankm335@gmail.com>
2024-05-03 12:11:04 +0530

    including assets file

--- app/index.js
+++ app/index.js
@@ -5,18 +5,9 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-blue-500">
-      <Text className="text-white text-2xl">App Learning 1 2 3 4 5</Text>
+      <Text className="text-white text-2xl font-pblack">App Learning 1 2 3 4 5</Text>
       <StatusBar style="auto" />
       <Link href={"/profile"} className="text-white underline">Go to Profile</Link>
     </View>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
