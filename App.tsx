e92b4b230a77e8439789e60ff60fb2b3aac11ab5
ankr19 <ankm335@gmail.com>
2026-01-30 14:31:12 +0530

    build: native wind works with react native.

--- App.tsx
+++ App.tsx
@@ -1,18 +1,15 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/home-srceen';
-
-// function HomeScreen() {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center',backgroundColor: 'red' }}>
-//       <Text style={{color: 'white'}}>Home Screen</Text>
-//     </View>
-//   );
-// }
+import "./global.css";
 
 const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Home',
+  screenOptions: {
+    // headerStyle: { backgroundColor: 'tomato' },
+    headerShown: false,
+  },
   screens: {
     Home: HomeScreen,
   },
--- src/screens/home-srceen.tsx
+++ src/screens/home-srceen.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function HomeScreen() {
   return (
-    <View>
+    <SafeAreaView className="bg-gray-200 flex-1">
       <Text>HomeScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
